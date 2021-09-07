56b13eeed968b4892576c5277a275ac5bce2dcf0
HeikoMatternTK <heiko.mattern@turbinekreuzberg.com>
2021-09-07 16:41:35 +0200

    Working templating, added song lyrics

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const path = require('path')
+const fs = require('fs');
 
 const app = express();
 
 // set templating engine
-app.set('view engine', 'html');
+app.set('view engine', 'pug');
 app.set('views', __dirname);
 
 // configure static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // app index
-app.get('/', function(req, res) {
-  res.send('Hello');
+// app.get('/', function(req, res) {
+//  res.send('Hello');
+// });
+
+app.get('/', function (req, res) {
+  const song = JSON.parse(fs.readFileSync('./public/come_together.json'));
+  res.render('index', song);
 });
 
 // Heroku uses dynamic port binding
